0906c15b17bc9c793600d9aaa32fa6c55dec5f80
GregoireLambrecht <gregoire-lambrecht@outlook.fr>
2022-03-16 15:05:21 +0100

    PuitsSave

--- PuitsPotentiel.jl
+++ PuitsPotentiel.jl
@@ -1,7 +1,6 @@
 include("lobpcg.jl")
 using FFTW
 using Plots
-using KrylovKit
 using LinearAlgebra
 
 mutable struct Params
@@ -10,6 +9,7 @@ mutable struct Params
 	k_axis
 	k2_grid
 	k2lin
+	solver
 end
 
 function init_struct(n,dim)
@@ -29,7 +29,7 @@ function init_struct(n,dim)
 		k2_grid = norm.(k2_grid).^2
 		k2lin = linearize(k2_grid)
 	end
-	Params(dim,n,k_axis,k2_grid,k2lin)
+	Params(dim,n,k_axis,k2_grid,k2lin,"lobpcg")
 end
 
 linearize(X) = vcat(X...)
@@ -41,37 +41,47 @@ convolve(X,V) = fft(ifft(X).*ifft(V))# X et V en fourier
 #k : mode recherché, doit être plus petit que 5 et plus grand que 1
 function solve(V,p,k)
 	N = p.n^p.dim
-	VLinFour = linearize(fft(V))
-	H = X->4*pi^2 .*p.k2lin.*X + convolve(VLinFour,X) # X est en fourier et linéraire
-	l = 5 # nb modes propres
+	V_four = fft(V)
+	H = X->4*pi^2 .*p.k2lin.*X .+ linearize(convolve(V_four,Reshape(X,p))) # X est en fourier et linéraire
+	l = k+1 # nb modes propres
+	#if p.solver == "lobpcg"
 	(λs,ϕs,cv) = solve_lobpcg(H,N,l,p.k2lin;tol=1e-7)
-	#(λs,ϕs) = eigsolve(H,p.k2lin,l)
+	#else
+		#eis = []
+		#[H(ei) for ei in ]
+		#(λs,ϕs) = eigen(Array(H))
+	#end
 	# k ieme etat excite
 	ψ = ϕs[k]; Eψ = λs[k]
 	ψ = Reshape(ψ,p)
 	Mψ = ifft(ψ)
-	Mψ = real.(conj.(Mψ) .* Mψ)
 	Mψ ./= norm(Mψ)
 	(Eψ,Mψ)
 end
 
-#Une dimension
-#p = init_struct(200,1)
-#x = [(i/p.n) for i=1:p.n]
-#V = -200*exp.((-(x.-0.5).^2)./ 0.1)
-#(E,ϕ) = solve(V,p,2)
+#Une dimension. x pour l'abscisse, V pour le potentiel, k pour le mode
+function D1(x,V,k)
+	p = init_struct(100,1)
+	(E,ϕ) = solve(V,p,k)
+	pl = plot(x,abs2.(ϕ))
+	savefig(pl,"pot1d.pdf")
+end
 
-#plot(x,ϕ)
+x = [(i/p.n) for i=1:p.n]
+D1(x,-200*exp.((-(x.-0.5).^2)./ 0.1),4)
+
+#2D, y pour l'ordonnée, V pour le potentiel (tenseur d'ordre 2)
+function D2(x,y,V,k)
+	p = init_struct(100,2)
+	(E,ϕ) = solve(V,p,k)
+	pl = Plots.heatmap(x,y,abs2.(ϕ))
+	surf = Plots.surface(x,y,abs2.(ϕ))
+	savefig(pl,"pot2dheat.pdf")
+	savefig(surf,"pot2surf.pdf")
+end
 
 function f(x,y)
 	-2000*exp(-((x-0.5)^2+(y-0.5)^2)/0.1)
 end
 
-#plot!(x,V)
-#Deux dimensions
-p = init_struct(100,2)
-x = [((i-1)/p.n) for i=1:p.n]
-y = [((i-1)/p.n) for i=1:p.n]
-VS = linearize([ f(x[i],y[j]) for i=1:p.n, j=1:p.n])
-(ES,ϕS) = solve(VS,p,1)
-Plots.surface(x,y,ϕS)
+D2(x,x,[ f(x[i],y[j]) for i=1:p.n, j=1:p.n],3)
